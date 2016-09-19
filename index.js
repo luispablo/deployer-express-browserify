0f091fd849778926e45b99bf87ffbddbd4b0472a
lgallo <lgallo@tsjbaires.gov.ar>
2016-09-19 12:24:30 -0300

    Change tabs for spaces

--- index.js
+++ index.js
@@ -16,28 +16,28 @@ var config = JSON.parse(fs.readFileSync(DEPLOY_CONF_FILENAME, "utf8"));
 var sshConnection = new ssh2.Client();
 
 checks(NPM_CONF_FILENAME, DEPLOY_CONF_FILENAME)
-	.then(gatherInput(config.servers))
-	.then(function (input) {
-		console.log("Connecting to server", input.serverName);
-		sshConnection.on("ready", function () {
-			console.log("SSH connection ready");
-			transpile(config.entryFile, config.outputFile)
-				.then(uploadFiles(sshConnection, config.filesToUpload, process.cwd(), config.appDirInServer))
-				.then(promiseCommand(sshConnection, "cd "+ config.appDirInServer +" && npm install --production"))
-				.then(promiseCommand(sshConnection, config.restartCommand))
-				.then(function () {
-					sshConnection.end();
-					process.exit(0);
-				}).catch(function (error) {
-					sshConnection.end();
-					console.log(chalk.red(error.stack));
-					process.exit(1);
-				});
-		}).connect({
-			host: input.serverName, port: 22,
-			username: input.username, password: input.password
-		});
-	}).catch(function (error) {
-		console.log(chalk.red(error.stack));
-		process.exit(1);
-	});
+  .then(gatherInput(config.servers))
+  .then(function (input) {
+    console.log("Connecting to server", input.serverName);
+    sshConnection.on("ready", function () {
+      console.log("SSH connection ready");
+      transpile(config.entryFile, config.outputFile)
+        .then(uploadFiles(sshConnection, config.filesToUpload, process.cwd(), config.appDirInServer))
+        .then(promiseCommand(sshConnection, "cd "+ config.appDirInServer +" && npm install --production"))
+        .then(promiseCommand(sshConnection, config.restartCommand))
+        .then(function () {
+          sshConnection.end();
+          process.exit(0);
+        }).catch(function (error) {
+          sshConnection.end();
+          console.log(chalk.red(error.stack));
+          process.exit(1);
+        });
+    }).connect({
+      host: input.serverName, port: 22,
+      username: input.username, password: input.password
+    });
+  }).catch(function (error) {
+    console.log(chalk.red(error.stack));
+    process.exit(1);
+  });
